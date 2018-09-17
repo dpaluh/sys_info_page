0f248909acd73c7464289c2beb9f918953cb2e0e
Daniel Paluh <dpaluh@login2.ufhpc>
2018-09-17 12:05:05 -0400

    Adding functions

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -5,6 +5,30 @@ TITLE="System Information Report For $HOSTNAME"
 CURRENT_TIME="$(date +"%x %r %Z")"
 TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
+report_uptime () {
+	cat <<- _EOF_
+	<H2>System Uptime</H2>
+	<PRE>$(uptime)</PRE>
+	_EOF_
+return 
+}
+
+report_disk_space () {
+	cat<<- _EOF_
+	<H2>Disk Space Utilization</H2>
+	<PRE>$(df -h)</PRE>
+	_EOF_
+return 
+}
+
+report_home_space () {
+	cat<<- _EOF_
+	<H2>Home Space Utilization</H2>
+	<pre>$(du -sh /home/dpaluh/*)</pre>
+	_EOF_
+return 
+}
+
 cat << _EOF_
 <HTML>
     <HEAD>
@@ -13,6 +37,9 @@ cat << _EOF_
     <BODY>
         <H1>$TITLE</H1>
 	<P>$TIMESTAMP</P>
-    </BODY>
+	$(report_uptime)
+        $(report_disk_space)
+        $(report_home_space)
+ </BODY>
 </HTML>
 _EOF_
